2a760b0f6a06fb5053c67074f3988d93fa1790de
James Henderson <james@pcr.me.uk>
2013-03-30 12:36:32 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarohen/nomad "0.1.0-SNAPSHOT"
+  :description "A Clojure library to allow Clojure applications to define and access host/instance-specific configuration"
+  :url "https://github.com/james-henderson/nomad.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
