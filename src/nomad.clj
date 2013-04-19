b3337aab51b829cf2433c07f53f0b07f80466913
James Henderson <james@pcr.me.uk>
2013-04-19 10:22:36 +0100

    Having a bash at defconfig - need to test this

--- src/nomad.clj
+++ src/nomad.clj
@@ -2,41 +2,69 @@
   (:require [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]))
 
-(defn- config-file []
-  (io/file (io/resource "nomad-config.edn")))
+(defprotocol ConfigFile
+  (etag [_])
+  (exists? [_]))
 
-(defn- load-config []
-  (let [file (config-file)]
-    (when (and file (.exists file))
-      (with-meta (read-string (slurp file))
-        {:as-of (.lastModified file)}))))
+(extend-protocol ConfigFile
+  java.io.File
+  (etag [f] (.lastModified f))
+  (exists? [f] (.exists f))
 
-(def ^:private config (ref nil))
+  java.net.URL
+  (etag [url]
+    (case (.getProtocol url)
+      "file" (etag (io/as-file url))
 
-(defn get-config []
+      ;; otherwise, we presume the config file is read-only
+      ;; (i.e. in a JAR file)
+      url))
+  (exists? [url]
+    ;; any offers of a better implementation for this?
+    (try
+      (with-open [s (.openStream url)]
+        true)
+      (catch Exception e
+        false)))
+
+  nil
+  (etag [_] nil)
+  (exists? [_] false))
+
+(defn- load-config [config-file]
+  (when (exists? config-file)
+    (with-meta (read-string (slurp config-file))
+      {:etag (etag config-file)})))
+
+(defn get-config [config-ref config-file]
   (dosync
-   (alter config
+   (alter config-ref
           (fn [current-config]
             (if-not current-config
-              (load-config)
+              (load-config config-file)
               
-              (let [file-mod (.lastModified (config-file))]
-                (if (not= file-mod (-> current-config meta :as-of))
-                  (load-config)
-                    current-config)))))))
+              (let [file-etag (etag config-file)]
+                (if (not= file-etag (-> current-config meta :etag))
+                  (load-config config-file)
+                  current-config)))))))
 
 (def ^:private get-hostname
   (memoize
    (fn []
      (.trim (:out (sh "hostname"))))))
 
-(defn get-host-config []
-  (get-in (get-config) [:hosts (get-hostname)]))
+(defn get-host-config [config]
+  (get-in config [:nomad/hosts (get-hostname)]))
 
 (def ^:private get-instance
   (memoize
    (fn []
      (get (System/getenv) "NOMAD_INSTANCE" :default))))
 
-(defn get-instance-config []
-  (get-in (get-host-config) [:instances (get-instance)]))
+(defn get-instance-config [config]
+  (get-in (get-host-config config) [:nomad/instances (get-instance)]))
+
+(defmacro defconfig [name file-or-resource]
+  `(let [config-ref# (ref nil)]
+     (defn ~name []
+       (get-config config-ref# ~file-or-resource))))
