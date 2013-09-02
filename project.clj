f589dcfdafdafab6b19512cd5a9fe682709e0737
James Henderson <james@pcr.me.uk>
2013-09-02 17:24:56 +0100

    And upping to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/nomad "0.5.1"
+(defproject jarohen/nomad "0.5.2-SNAPSHOT"
   :description "A Clojure library to allow Clojure applications to define and access host/instance-specific configuration"
   :url "https://github.com/james-henderson/nomad.git"
   :license {:name "Eclipse Public License"
