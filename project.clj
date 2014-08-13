25f4b474a6a98b9376e323ad5fff13a5dbf0bf99
James Henderson <james@jarohen.me.uk>
2014-08-13 18:30:39 +0100

    Bumping to rc2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/nomad "0.6.5-rc1"
+(defproject jarohen/nomad "0.6.5-rc2"
   :description "A Clojure library to allow Clojure applications to define and access host/instance-specific configuration"
   :url "https://github.com/james-henderson/nomad.git"
   :license {:name "Eclipse Public License"
