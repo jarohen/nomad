7c6fcdb5637d5bcfb298b2c89c5e34d081248f1f
James Henderson <james@jarohen.me.uk>
2015-06-07 12:16:06 +0100

    Going to beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/nomad "0.7.1"
+(defproject jarohen/nomad "0.8.0-beta1"
   :description "A Clojure library to allow Clojure applications to define and access host/instance-specific configuration"
   :url "https://github.com/james-henderson/nomad.git"
   :license {:name "Eclipse Public License"
