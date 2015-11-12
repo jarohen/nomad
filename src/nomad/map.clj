df667b5d8389603a5365db8740aafc58a388e834
ganmacs <ganmacs@gmail.com>
2015-11-13 00:41:48 +0900

    refactor the implement and add test

--- src/nomad/map.clj
+++ src/nomad/map.clj
@@ -7,9 +7,6 @@
               {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
   -> {:a {:b {:c 2, :d {:x 1, :y 2, :z 9}, :z 3}, :e 100}, :f 4}"
   [& maps]
-  (apply
-    (fn m [& maps]
-      (if (every? map? maps)
-        (apply merge-with m maps)
-        (last maps)))
-    maps))
+  (if (every? map? maps)
+    (apply merge-with deep-merge maps)
+    (last maps)))
