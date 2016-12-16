1733783d169d15f7da59098aaf19b8e97c405aac
James Henderson <james@jarohen.me.uk>
2016-12-16 17:34:02 +0000

    Release 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/nomad "0.7.2"
+(defproject jarohen/nomad "0.7.3"
   :description "A Clojure library to allow Clojure applications to define and access host/instance-specific configuration"
   :url "https://github.com/james-henderson/nomad.git"
   :license {:name "Eclipse Public License"
