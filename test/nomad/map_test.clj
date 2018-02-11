98af213ed0a4e8b00b9b52ed2cc2a2c8cd5f690b
James Henderson <james@jarohen.me.uk>
2018-02-11 21:32:36 +0000

    Clearing out the README + adding deprecations in preparation for the big 0.9.0 rethink

--- test/nomad/map_test.clj
+++
@@ -1,9 +0,0 @@
-(ns nomad.map-test
-  (:require [nomad.map :refer :all]
-            [clojure.test :refer :all]))
-
-(deftest deep-merge-test
-  (let [v1 {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
-        v2 {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}}]
-    (is (= {:a {:b {:c 2, :d {:x 1, :y 2, :z 9}, :z 3}, :e 100}, :f 4}
-           (deep-merge v1 v2)))))
