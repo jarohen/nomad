33990b91d204b7f73688c9dca75b2c8957168a8e
James Henderson <james@jarohen.me.uk>
2018-07-17 21:35:46 +0100

    To deps.edn

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject jarohen/nomad "0.9.0-SNAPSHOT"
-  :description "A configuration library designed to allow Clojure applications to travel painlessly between different hosts and environments."
-  :url "https://github.com/jarohen/nomad"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.reader "1.2.1"]
-                 [buddy/buddy-core "1.4.0"]])
