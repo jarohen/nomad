1b6dd73e742b49cd697c355444ff2c2161f2873e
James Henderson <james@jarohen.me.uk>
2019-01-23 20:27:44 +0000

    cheeky data-readers

--- src/data_readers.clj
+++
@@ -1,8 +0,0 @@
-{nomad/file nomad.temp-080-beta.readers/file-reader
- nomad/resource nomad.temp-080-beta.readers/resource-reader
- nomad/env-var nomad.temp-080-beta.readers/env-var-reader
- nomad/edn-env-var nomad.temp-080-beta.readers/edn-env-var-reader
- nomad/jvm-prop nomad.temp-080-beta.readers/jvm-prop-reader
- nomad/edn-jvm-prop nomad.temp-080-beta.readers/edn-jvm-prop-reader
- nomad/secret nomad.temp-080-beta.readers/secret-reader
- nomad/snippet nomad.temp-080-beta.readers/snippet-reader}
